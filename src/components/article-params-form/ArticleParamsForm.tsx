9b85391e36507f571925f68717c49d22615a1bae
Deluxo <deluxo31@gmail.com>
2025-11-24 16:43:09 +0300

    fix: functionality of the implemented work logic. Added state components and event handlers, and correctly passed data between components

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,14 +1,121 @@
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
-
 import styles from './ArticleParamsForm.module.scss';
+import { useRef, useEffect } from 'react';
+import { RadioGroup } from 'src/ui/radio-group/RadioGroup';
+import { Select } from 'src/ui/select/Select';
+import {
+	ArticleStateType,
+	fontFamilyOptions,
+	fontSizeOptions,
+	fontColors,
+	backgroundColors,
+	contentWidthArr,
+} from 'src/constants/articleProps';
+import clsx from 'clsx';
 
-export const ArticleParamsForm = () => {
+type ArticleParamsFormProps = {
+	isOpen: boolean;
+	onToggle: () => void;
+	formState: ArticleStateType;
+	onFormChange: (newState: ArticleStateType) => void;
+	onApply: () => void;
+	onReset: () => void;
+};
+
+export const ArticleParamsForm = ({
+	isOpen,
+	onToggle,
+	formState,
+	onFormChange,
+	onApply,
+	onReset,
+}: ArticleParamsFormProps) => {
+	const sidebarRef = useRef<HTMLElement>(null);
+
+	useEffect(() => {
+		const handleClickOutside = (e: MouseEvent) => {
+			if (!sidebarRef.current?.contains(e.target as Node) && isOpen) {
+				onToggle();
+			}
+		};
+		const handleEscape = (e: KeyboardEvent) => {
+			if (e.key === 'Escape' && isOpen) {
+				onToggle();
+			}
+		};
+
+		document.addEventListener('mousedown', handleClickOutside);
+		document.addEventListener('keydown', handleEscape);
+
+		return () => {
+			document.removeEventListener('mousedown', handleClickOutside);
+			document.removeEventListener('mousedown', handleClickOutside);
+		};
+	}, [isOpen, onToggle]);
+
+	const handleSubmit = (e: React.FormEvent) => {
+		e.preventDefault();
+		onApply();
+	};
+
+	const handleResetForm = (e: React.FormEvent) => {
+		e.preventDefault();
+		onReset();
+	};
+
+	const updateFormState = (
+		key: keyof ArticleStateType,
+		value: ArticleStateType[keyof ArticleStateType]
+	) => {
+		onFormChange({
+			...formState,
+			[key]: value,
+		});
+	};
 	return (
 		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
-				<form className={styles.form}>
+			<ArrowButton isOpen={isOpen} onClick={onToggle} />
+			<aside
+				ref={sidebarRef}
+				className={clsx(styles.container, {
+					[styles.container_open]: isOpen,
+				})}>
+				<form
+					className={styles.form}
+					onSubmit={handleSubmit}
+					onReset={handleResetForm}>
+					<Select
+						selected={formState.fontFamilyOption}
+						options={fontFamilyOptions}
+						onChange={(option) => updateFormState('fontFamilyOption', option)}
+						title='ШРИФТ'
+					/>
+					<RadioGroup
+						name='font-size'
+						options={fontSizeOptions}
+						selected={formState.fontSizeOption}
+						onChange={(option) => updateFormState('fontSizeOption', option)}
+						title='РАЗМЕР ШРИФТА'
+					/>
+					<Select
+						selected={formState.fontColor}
+						options={fontColors}
+						onChange={(option) => updateFormState('fontColor', option)}
+						title='ЦВЕТ ШРИФТА'
+					/>
+					<Select
+						selected={formState.backgroundColor}
+						options={backgroundColors}
+						onChange={(option) => updateFormState('backgroundColor', option)}
+						title='ЦВЕТ ФОНА'
+					/>
+					<Select
+						selected={formState.contentWidth}
+						options={contentWidthArr}
+						onChange={(option) => updateFormState('contentWidth', option)}
+						title='ШИРИНА КОНТЕНТА'
+					/>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
 						<Button title='Применить' htmlType='submit' type='apply' />
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
 import { createRoot } from 'react-dom/client';
-import { StrictMode, CSSProperties } from 'react';
+import { StrictMode, CSSProperties, useState } from 'react';
 import clsx from 'clsx';
 
 import { Article } from './components/article/Article';
 import { ArticleParamsForm } from './components/article-params-form/ArticleParamsForm';
-import { defaultArticleState } from './constants/articleProps';
+import {
+	defaultArticleState,
+	ArticleStateType,
+} from './constants/articleProps';
 
 import './styles/index.scss';
 import styles from './styles/index.module.scss';
@@ -13,19 +16,51 @@ const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
 
 const App = () => {
+	const [articleState, setArticleState] = useState(defaultArticleState); // Текущие стили
+	const [formState, setFormState] = useState(defaultArticleState); // Временные настройки в форме
+	const [isOpen, setIsOpen] = useState(false); //  Форма открыта или закрыта
+
+	// функия применения стилей
+	const handleApplyArticle = () => {
+		setArticleState(formState);
+		setIsOpen(false);
+	};
+
+	// функция для сброса к стандартным настройкам
+	const handleResetArticle = () => {
+		setArticleState(defaultArticleState);
+		setFormState(defaultArticleState);
+		setIsOpen(false);
+	};
+
+	// функция обновления формы
+	const handleFormChange = (newState: ArticleStateType) => {
+		setFormState(newState);
+	};
+	//}
+	//открыть закрыть
+	const switchOpenClose = () => setIsOpen(!isOpen);
+
 	return (
 		<main
 			className={clsx(styles.main)}
 			style={
 				{
-					'--font-family': defaultArticleState.fontFamilyOption.value,
-					'--font-size': defaultArticleState.fontSizeOption.value,
-					'--font-color': defaultArticleState.fontColor.value,
-					'--container-width': defaultArticleState.contentWidth.value,
-					'--bg-color': defaultArticleState.backgroundColor.value,
+					'--font-family': articleState.fontFamilyOption.value,
+					'--font-size': articleState.fontSizeOption.value,
+					'--font-color': articleState.fontColor.value,
+					'--container-width': articleState.contentWidth.value,
+					'--bg-color': articleState.backgroundColor.value,
 				} as CSSProperties
 			}>
-			<ArticleParamsForm />
+			<ArticleParamsForm
+				isOpen={isOpen}
+				onToggle={switchOpenClose}
+				formState={formState}
+				onFormChange={handleFormChange}
+				onApply={handleApplyArticle}
+				onReset={handleResetArticle}
+			/>
 			<Article />
 		</main>
 	);
